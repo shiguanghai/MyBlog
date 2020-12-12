2d2617f48312d505aafc76ebfdd249f25eb554be
shiguanghai <1932275668@qq.com>
2020-12-12 16:33:49 +0800

    持续化部署

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@
 # 打包 
 # npm run build
 # 删除 ngnix 指向的文件夹下得文件
-rm -rf /usr/share/nginx/html
+rm -rf /usr/share/nginx/html/*
 
 # 将打包好的文件复制过去
-mv /tmp/MyBlog/dist/* /usr/share/nginx/html
+mv /tmp/MyBlog/dist/*  /usr/share/nginx/html
 # 删除 clone 的代码
 rm -rf /tmp/MyBlog
